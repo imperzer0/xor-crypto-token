ca7e70a244ec731610b522e1421355eafc98e84d
imperzer0 <imperator999mcpe@gmail.com>
2021-06-19 16:37:02 +0300

    completions bug fixed

--- main.cpp
+++ main.cpp
@@ -642,7 +642,7 @@ int main(int argc, char** argv)
 		set_completion(argv[2], "passwd", new const char* []{ }, 0, "password", "--action=create-token");
 		set_completion(argv[2], "passwd-file", new const char* []{"(ls -p | grep -v /)"}, 1, "file with password", "--action=create-token");
 		set_completion(argv[2], "src", new const char* []{"(ls -p /dev/sd?)"}, 1, "source token device", "--action=copy-token");
-		set_completion(argv[2], "dest", new const char* []{"(ls -p /dev/sd?"}, 1, "destination token device", "--action=copy-token");
+		set_completion(argv[2], "dest", new const char* []{"(ls -p /dev/sd?)"}, 1, "destination token device", "--action=copy-token");
 	}
 	else if ((action == "uninstall-completions") && argc == 3)
 	{
